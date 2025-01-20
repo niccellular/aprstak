1ba1904be124d77c112809586e55cd43836643d4
niccellular <79813408+niccellular@users.noreply.github.com>
2025-01-20 11:45:20 -0500

    3.0
    removed libcotshrink

--- app/src/main/java/com/atakmap/android/aprstak/APRSMessageHandler.java
+++ app/src/main/java/com/atakmap/android/aprstak/APRSMessageHandler.java
@@ -9,10 +9,17 @@ import android.util.Log;
 
 import com.atakmap.comms.CommsMapComponent;
 import com.atakmap.coremap.cot.event.CotEvent;
-import com.paulmandal.atak.libcotshrink.pub.api.CotShrinker;
 
 import com.atakmap.android.aprstak.plugin.PluginLifecycle;
+import com.siemens.ct.exi.core.EXIFactory;
+import com.siemens.ct.exi.core.helpers.DefaultEXIFactory;
+import com.siemens.ct.exi.main.api.sax.EXIResult;
 
+import org.xml.sax.InputSource;
+import org.xml.sax.XMLReader;
+
+import java.io.ByteArrayOutputStream;
+import java.io.StringReader;
 import java.nio.ByteBuffer;
 import java.security.MessageDigest;
 import java.security.SecureRandom;
@@ -20,26 +27,47 @@ import java.security.SecureRandom;
 import javax.crypto.Cipher;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
 
 import utils.AprsUtility;
 
 public class APRSMessageHandler implements CommsMapComponent.PreSendProcessor {
 
     private static final String TAG = "APRSMessageHandler";
-    private final CotShrinker cotShrinker;
+
     private Context context;
 
-    public APRSMessageHandler(CotShrinker cotShrinker, Context context) {
+    public APRSMessageHandler(Context context) {
         CommsMapComponent.getInstance().registerPreSendProcessor(this);
-        this.cotShrinker = cotShrinker;
         this.context = context;
     }
 
     @Override
     public void processCotEvent(CotEvent cotEvent, String[] toUIDs) {
         if (cotEvent.toString().contains("All Chat Rooms")) {
-            byte[] cotBytes = cotShrinker.toByteArrayLossy(cotEvent);
-            String encodedString;
+            byte[] cotAsBytes;
+
+            try {
+                EXIFactory exiFactory = DefaultEXIFactory.newInstance();
+                ByteArrayOutputStream osEXI = new ByteArrayOutputStream();
+                EXIResult exiResult = new EXIResult(exiFactory);
+                exiResult.setOutputStream(osEXI);
+                SAXParserFactory saxParserFactory = SAXParserFactory.newInstance();
+                SAXParser newSAXParser = saxParserFactory.newSAXParser();
+                XMLReader xmlReader = newSAXParser.getXMLReader();
+                xmlReader.setContentHandler(exiResult.getHandler());
+                InputSource stream = new InputSource(new StringReader(cotEvent.toString()));
+                xmlReader.parse(stream); // parse XML input
+                cotAsBytes = osEXI.toByteArray();
+                osEXI.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+                return;
+            }
+
+            Log.d(TAG, "Size: " + cotAsBytes.length);            String encodedString;
+
             if (AprsUtility.usePSK) {
                 com.atakmap.coremap.log.Log.i(TAG, "PSK enabled");
                 ByteBuffer payload;
@@ -59,7 +87,7 @@ public class APRSMessageHandler implements CommsMapComponent.PreSendProcessor {
                     Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
                     SecretKeySpec key = new SecretKeySpec(PSKhash, "AES");
                     cipher.init(Cipher.ENCRYPT_MODE, key, new IvParameterSpec(iv));
-                    cipherText = cipher.doFinal(cotBytes);
+                    cipherText = cipher.doFinal(cotAsBytes);
                     // set the iv+cipherText as the payload
                     payload = ByteBuffer.allocate(iv.length + cipherText.length);
                     payload.put(iv);
@@ -70,7 +98,7 @@ public class APRSMessageHandler implements CommsMapComponent.PreSendProcessor {
                 }
                 encodedString = Base64.encodeToString(payload.array(), Base64.NO_WRAP);
             } else {
-                encodedString = Base64.encodeToString(cotBytes, Base64.NO_WRAP);
+                encodedString = Base64.encodeToString(cotAsBytes, Base64.NO_WRAP);
             }
 
             Log.d(TAG, "Base64 string len: " + encodedString.length());
